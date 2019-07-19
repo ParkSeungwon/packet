df4b7426cd4533893a60a64f33287f653f775d38
zeta <zezeon@msn.com>
2019-07-20 00:15:19 +0530

    2019. 07. 20. (토) 00:15:19 +0530 autocommit

--- src/test.cpp
+++ src/test.cpp
@@ -5,7 +5,8 @@ int main()
 {
 	Packet packet;
 	packet.show();
-	packet.ethernet_header.ether_dhost = {0x00,0x1d,0xbc,0x49,0x86,0x15};
+ 	uint8_t dh[] = {0x00,0x1d,0xbc,0x49,0x86,0x15};
+	for(int i=0; i<6; i++) packet.ethernet_header.ether_dhost[i] = dh[i];
 	packet.set_ipdst("localhost");
 	packet.tcp_header.th_dport = htons(2001);
 	strcpy(packet.data, "th is isadata");
